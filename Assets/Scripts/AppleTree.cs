a97857db104f1497a572bd37ce9c194f52e677c3
Tomislav Kristo <Tomkristo@gmail.com>
2017-09-20 13:12:21 -0400

    Added basic movment and directional change to AppleTree

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,16 +1,55 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Initiating apples
+    public GameObject applePrefab;
+
+    //Speed AppleTree moves (meters/second)
+    public float speed = 1f;
+
+    //Distance where AppleTree changes direction
+    public float leftAndRightEdge = 10f;
+
+    //Chance AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Apple spawn rate
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Dropping Apples every second
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing directions
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move Right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //Move left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Changing Directions Randomly
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change directions
+        }
+    }
 }
